57f31f7755a2b047e460b3da0fa5776be28bd83d
Dongho Gwak <wing.flower.k@gmail.com>
2024-01-12 17:11:46 +0900

    fix: TextField 입력으로 ListView에 출력

--- lib/detail/list_screen.dart
+++ lib/detail/list_screen.dart
@@ -8,24 +8,86 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
-  final int _printCount = 20;
+  final outputTextEditingController = TextEditingController();
+  final countTextEditingController = TextEditingController();
+
+  String _printCount = '3';
+  String _printText = '아버지';
+
+  @override
+  void dispose() {
+    outputTextEditingController.dispose();
+    countTextEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.list_alt),
-          ),
-        ],
-      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            BottomListContainer(_printCount),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: outputTextEditingController,
+                    decoration: InputDecoration(
+                      hintText: '아버지',
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(
+                          width: 2,
+                          color: Colors.black,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(
+                          width: 2,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: TextField(
+                    controller: countTextEditingController,
+                    decoration: InputDecoration(
+                      hintText: '3',
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(
+                          width: 2,
+                          color: Colors.black,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(
+                          width: 2,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _printText = outputTextEditingController.text ?? 'Test';
+                      _printCount = countTextEditingController.text ?? '3';
+                    });
+                  },
+                  icon: const Icon(Icons.list_alt),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            BottomListContainer(_printText, int.parse(_printCount)),
           ],
         ),
       ),
@@ -33,23 +95,21 @@ class _ListScreenState extends State<ListScreen> {
   }
 }
 
-
 class BottomListContainer extends StatelessWidget {
+  final String text;
   final int count;
 
-  const BottomListContainer(this.count, {super.key});
+  const BottomListContainer(this.text, this.count, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.black)
-        ),
+        decoration: BoxDecoration(border: Border.all(color: Colors.black)),
         child: ListView.builder(
           itemCount: count,
           itemBuilder: (context, index) {
-            return Text('${index + 1}');
+            return Text('${index + 1} $text');
           },
         ),
       ),
