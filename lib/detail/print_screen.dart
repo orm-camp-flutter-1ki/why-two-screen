8e3f4593d9a0feacee748825ce12e09c0b0b41c5
eunjinjeong <eunjin.jeong.5115@gmail.com>
2024-01-12 19:37:27 +0900

    출력화면 UI 완성

--- lib/detail/print_screen.dart
+++ lib/detail/print_screen.dart
@@ -8,20 +8,52 @@ class PrintScreen extends StatefulWidget {
 }
 
 class _PrintScreenState extends State<PrintScreen> {
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text(
-            '리스트 화면',
-          ),
-          ),
-
-      );
-
+        title: const Text(
+          '출력 화면',
+        ),
+      ),
+      body: Column(
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            SizedBox(
+              width: 100,
+              height: 50,
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.purple.shade100,
+                ),
+              ),
+            ),
+            const SizedBox(width: 8),
+            SizedBox(
+              width: 100,
+              height: 50,
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.purple.shade100,
+                ),
+              ),
+            ),
+            const SizedBox(width: 8),
+            ElevatedButton(onPressed: () {}, child: const Text('출력'))
+          ]),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.all(16),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
