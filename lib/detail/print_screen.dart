566a32a47b0af2a86a8be090eb042aab568633bf
eunjinjeong <eunjin.jeong.5115@gmail.com>
2024-01-12 20:26:56 +0900

    feat: 출력화면 기능 완성

--- lib/detail/print_screen.dart
+++ lib/detail/print_screen.dart
@@ -8,6 +8,16 @@ class PrintScreen extends StatefulWidget {
 }
 
 class _PrintScreenState extends State<PrintScreen> {
+  final TextEditingController _textController = TextEditingController();
+  final TextEditingController _numberController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    _numberController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,44 +26,57 @@ class _PrintScreenState extends State<PrintScreen> {
           '출력 화면',
         ),
       ),
-      body: Column(
-        children: [
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            SizedBox(
-              width: 100,
-              height: 50,
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.purple.shade100,
-                ),
+      body: Column(children: [
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          SizedBox(
+            width: 100,
+            height: 50,
+            child: TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.purple.shade100,
               ),
             ),
-            const SizedBox(width: 8),
-            SizedBox(
-              width: 100,
-              height: 50,
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.purple.shade100,
-                ),
+          ),
+          const SizedBox(width: 8),
+          SizedBox(
+            width: 100,
+            height: 50,
+            child: TextField(
+              controller: _numberController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.purple.shade100,
               ),
             ),
-            const SizedBox(width: 8),
-            ElevatedButton(onPressed: () {}, child: const Text('출력'))
-          ]),
-          Expanded(
-            child: Container(
-              margin: const EdgeInsets.all(16),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-              ),
+          ),
+          const SizedBox(width: 8),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _textController.text;
+                });
+                // print(_textController.text);
+                // print(int.parse(_numberController.text));
+              },
+              child: const Text('출력'))
+        ]),
+        Expanded(
+          child: Container(
+            margin: const EdgeInsets.all(16),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
             ),
-          )
-        ],
-      ),
+            child: ListView.builder(
+                itemCount: int.tryParse(_numberController.text),
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(title: Text('${_textController.text}'));
+                }),
+          ),
+        ),
+      ]),
     );
   }
 }
