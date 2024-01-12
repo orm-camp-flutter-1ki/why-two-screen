c40aa394a92350aa16e7aad04cd13a0e32e8fd40
Dongho Gwak <wing.flower.k@gmail.com>
2024-01-12 15:50:37 +0900

    fix: Widget 분리

--- lib/detail/list_screen.dart
+++ lib/detail/list_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ListScreen extends StatelessWidget {
+class ListScreen extends StatefulWidget {
   const ListScreen({super.key});
+
+  @override
+  State<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  final int _printCount = 20;
 
   @override
   Widget build(BuildContext context) {
@@ -16,16 +23,34 @@ class ListScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Container(
-          decoration: BoxDecoration(
+        child: Column(
+          children: [
+            BottomListContainer(_printCount),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class BottomListContainer extends StatelessWidget {
+  final int count;
+
+  const BottomListContainer(this.count, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
             border: Border.all(color: Colors.black)
-          ),
-          child: ListView.builder(
-            itemCount: 99,
-            itemBuilder: (context, index) {
-              return Text('${index+1}');
-            },
-          ),
+        ),
+        child: ListView.builder(
+          itemCount: count,
+          itemBuilder: (context, index) {
+            return Text('${index + 1}');
+          },
         ),
       ),
     );
