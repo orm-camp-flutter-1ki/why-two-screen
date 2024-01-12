642cb622e813d5dce1c0e58afc5f465d4070f668
Sangin Han <hugedreamhd@gmail.com>
2024-01-12 14:55:31 +0900

    ListScreen에 body에 column으로 만들고 row안에 text 만들고 gesturedector로 감싸고 safearea하고  mainaxisalinment 하고, ontap내 프린트문으로 텍스트 출력 확인

--- lib/detail/list_screen.dart
+++ lib/detail/list_screen.dart
@@ -10,6 +10,30 @@ class ListScreen extends StatefulWidget {
 class _ListScreenState extends State<ListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      print('출력되나?');
+                    },
+                    child: Text(
+                      '출력',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
