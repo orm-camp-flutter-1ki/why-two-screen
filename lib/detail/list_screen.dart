3ce0598616cdd5682ca3149415e157fe74c62ebf
Sangin Han <hugedreamhd@gmail.com>
2024-01-12 15:17:25 +0900

    컨테이너를 하단에 만들고 전체를 차지하는 공간을 만듬

--- lib/detail/list_screen.dart
+++ lib/detail/list_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ListScreen extends StatefulWidget {
-  const ListScreen({super.key});
+  ListScreen({super.key});
 
   @override
   State<ListScreen> createState() => _ListScreenState();
+
+  final int _hundred = 100;
 }
 
 class _ListScreenState extends State<ListScreen> {
@@ -22,6 +24,7 @@ class _ListScreenState extends State<ListScreen> {
                   GestureDetector(
                     onTap: () {
                       print('출력되나?');
+                      const int = 100;
                     },
                     child: Text(
                       '출력',
@@ -29,6 +32,13 @@ class _ListScreenState extends State<ListScreen> {
                     ),
                   ),
                 ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                height: 100,//100인데 expanded해서 공간을 다 차지했나?
+                color: Colors.yellow,
               ),
             )
           ],
