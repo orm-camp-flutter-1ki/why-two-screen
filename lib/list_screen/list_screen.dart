b1d931111a8dbc23e85ec5d7bb6e3c1ec497d5c9
OSS0302 <tkdtn3264@gmail.com>
2024-01-12 17:36:49 +0900

    2. 리스트화면에서 출력버튼 누르면 1~100까지 출력

--- lib/list_screen/list_screen.dart
+++ lib/list_screen/list_screen.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
 
-class ListScreen extends StatelessWidget {
+class ListScreen extends StatefulWidget {
+
   const ListScreen({super.key});
 
+  @override
+  State<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  TextEditingController _textController = TextEditingController();
+  List<int> countList = [];
+
+
+  List<int> count() {
+    countList = List.generate(100, (index) => index + 1);
+    return countList;
+  }
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('ListScreen'),
       ),
-      body: Container(),
+      body: Align(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    count();
+                  });
+                },
+                child: const Text('출력')),
+            Expanded(
+              child: ListView.builder(
+                itemCount: countList.length,
+                itemBuilder: (BuildContext context, int count) {
+                  return Text('숫자출력 : $count  입니다. ');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
