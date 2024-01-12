079cc07156a2ea4ecb0e528ead24d9c0bfabf873
sangwoo <kajer102@gmail.com>
2024-01-12 15:45:40 +0900

    feat: 텍스트와 숫자를 입력하고 출력을 누르 리스트에 표시가 됩니다.

--- lib/detail/list_screen.dart
+++ lib/detail/list_screen.dart
@@ -9,41 +9,90 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
-  List<ListNumber> numberWidgets = [];
+  // List<ListNumber> numberWidgets = [];
 
-  void printNumber() {
-    numberWidgets = List.generate(100, (index) => ListNumber(number: index + 1));
-  }
+  String inputText = '';
+  int inputNumber = 0;
+
+  // void printNumber() {
+  //   // numberWidgets =
+  //   //     List.generate(100, (index) => ListNumber(number: index + 1));
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // appBar: AppBar(),
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      printNumber();
-                    });
-                  },
-                  child: const Text(
-                    '출력',
-                    style: TextStyle(color: Colors.black),
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          inputText = value;
+                          // print('text = $inputText');
+                        });
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'text',
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: ListView(
-                children: numberWidgets,
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {
+                        inputNumber = int.parse(value);
+                        // print('text = $inputNumber');
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'number',
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        //   // printNumber();
+                      });
+                    },
+                    child: const Text(
+                      '출력',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              Expanded(
+                // child: ListView(
+                //   children: numberWidgets,
+                // ),
+                child: ListView.builder(
+                    itemCount: inputNumber,
+                    itemBuilder: (context, index) {
+                      return Text(inputText);
+                    }),
+              ),
+            ],
+          ),
         ),
       ),
     );
